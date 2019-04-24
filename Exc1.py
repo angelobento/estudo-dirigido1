767b53af6a7f67165f79aea4b9dc7fb144b684fe
angelobento <49735006+angelobento@users.noreply.github.com>
2019-04-24 20:48:36 -0300

    exercicios da lista

---
+++ Exc1.py
@@ -0,0 +1,8 @@
+#1. Informe o valor lógico (True ou False) para as proposições lógicas abaixo:
+#a. ( ) 2 * 4 == 24/3
+#b. ( ) (15 % 4) < (19 % 6)
+#c. ( ) !(2 < 5) != (8 == 8)
+#d. ( ) True or False
+#e. ( ) 2**2 == math.pow(2, 2)
+#f. ( ) False and ( (7 / 2) > 3.5 )
+#g. ( ) ( math.ceil( 2.5 ) == 3) and (math.sqrt( 9) == 3)
---
+++ Exc5.py
@@ -0,0 +1,13 @@
+mercadoria = float(input(print("Qual o Valor da mercadoria: ")))
+porcentagem = float(input(print("Quantos porcentos será o desconto: ")))
+desconto = mercadoria * porcentagem / 100
+totaldesc = mercadoria - desconto
+
+print ("O valor do desconto é: ",desconto)
+print ("O valor total a pagar pela mercasalario = float(input(print("Qual o Valor do salario: ")))
+porcentagem = float(input(print("Quantos porcentos será o aumento: ")))
+aumento = salario * porcentagem / 100
+totalsal = salario + aumento
+
+print ("O valor do aumento é: ",aumento)
+print ("O valor total do salario é: ", totalsal)doria é: ", totaldesc)
